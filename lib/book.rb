14d47b32133eae7e8056aa260e6f277c780688b7
Seunggyu <seunggyul14@icloud.com>
2023-01-10 10:58:50 -0800

    commit tree c2970f150fb92d3272ed03adeec0b89106e499c7

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+require "pry"
 
+class Book 
+    attr_accessor :title, :author
+    def initialize(title, author)
+        @title = title
+        @author = author
+    end
 
+end
+
+new_book = Book.new("And Then There Were None", "Agatha Christie")
+# binding.pry
